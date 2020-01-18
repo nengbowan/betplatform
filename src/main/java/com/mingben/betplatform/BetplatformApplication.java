e8e6a3dee58a047809459163ecb43d082e45ade5
傅士勇 <nengbowan@163.com>
2020-01-18 18:56:57 +0800

    Initial commit

---
+++ src/main/java/com/mingben/betplatform/BetplatformApplication.java
@@ -0,0 +1,13 @@
+package com.mingben.betplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingben/betplatform/BetplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingben.betplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
